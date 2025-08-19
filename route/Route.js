cc2572b6baa61daa42e83106cd402c23af385d39
fernanda <20233005420@estudantes.ifpr.edu.br>
2025-08-19 18:52:39 -0300

    Delete route/Route.js

--- route/Route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser, getUsers } from "../controllers/Controller.js";
-
-const rota = Router();
-
-rota.post("/", createUser);
-rota.get("/", getUsers);
-
-export default rota;
