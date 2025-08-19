59431aa25c13a52809cf2b0576e5d47395f9f809
fer <20233005420@estudantes.ifpr.edu.br>
2025-08-19 16:06:25 -0300

    implementações

---
+++ bd/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwUtNhOJ5VYNVcm1dL_ft3F2PUkeYjq0o",
+  authDomain: "apiweb-4f392.firebaseapp.com",
+  projectId: "apiweb-4f392",
+  storageBucket: "apiweb-4f392.firebasestorage.app",
+  messagingSenderId: "103199073601",
+  appId: "1:103199073601:web:ae30967015bdf6c970edc0",
+  measurementId: "G-ST6RKM4T0V"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ route/route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getAllDados } from "../controllers/controller.js";
+
+const rota = Router();
+
+rota.get('/', getAllDados)
+
+export default rota;
